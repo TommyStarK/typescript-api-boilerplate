19c9671d13922c01d1fc5205a3351e2278233c15
Thomas Milox <thomasmilox@gmail.com>
2018-12-12 21:32:04 +0100

    Update utils.js

--- api/src/utils.js
+++ api/src/utils.js
@@ -1,102 +1,15 @@
-import fs from 'fs';
-import path from 'path';
+import crypto from 'crypto';
 
 export const utils = {
-  encodeBase64: async (file) => {
-    try {
-      const data = await utils.readFileAsync(file);
-      return Buffer.from(data).toString('base64');
-    } catch (error) {
-      throw (error);
-    }
-  },
-
   hash: (target) => {
-    const hash = require('crypto').createHash('sha256');
+    const hash = crypto.createHash('sha256');
     hash.update(target);
     return hash.digest('hex');
-  },
-
-  readdirAsync: (path) => {
-    return new Promise((resolve, reject) => {
-      fs.readdir(path, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  },
-
-  readFileAsync: (file) => {
-    return new Promise((resolve, reject) => {
-      fs.readFile(file, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  },
-
-  removeContentDirectory: async (target) => {
-    try {
-      const files = await utils.readdirAsync(target);
-      for (let file of files) {
-        const filePath = path.join(target, file);
-        const stat = await utils.statAsync(filePath);
-        if (stat.isFile(filePath)) {
-          await utils.unlinkAsync(filePath);
-        } else {
-          utils.removeContentDirectory(filePath);
-        }
-      }
-    } catch (error) {
-      throw (error);
-    }
-  },
-
-  statAsync: (path) => {
-    return new Promise((resolve, reject) => {
-      fs.stat(path, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  },
-
-  unlinkAsync: (path) => {
-    return new Promise((resolve, reject) => {
-      fs.unlink(path, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
   },
 
   validateEmail: (email) => {
     let re =
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return re.test(email)
-  },
-
-  writeFileAsync: (file, content) => {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(file, content, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
   }
 };
