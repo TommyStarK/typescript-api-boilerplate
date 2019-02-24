c4e0d6089d7e6c12aa7bc913a4e8bef695144599
TommyStarK <thomasmilox@gmail.com>
2019-02-24 16:49:09 +0100

    Fix integration test

--- test/integration.js
+++ test/integration.js
@@ -22,47 +22,52 @@ function createApp() {
 
 test.beforeEach(async (t) => {
   await mongo.connect(config.mongo);
-  // await mysql.connect(config.mysql);
+  await mysql.connect(config.mysql);
   await redis.connect(config.redis);
   const app = createApp();
   t.context.app = app;
 });
 
 test.serial('ping service', async (t) => {
-  t.plan(1);
   const {app} = t.context;
   const res = await request(app).get(`/${config.app.url}`);
   t.is(res.status, 200);
 });
 
 test.serial('404 not found', async (t) => {
-  t.plan(1);
   const {app} = t.context;
   const res = await request(app).get('/api.boilerplat');
   t.is(res.status, 404);
 });
 
 test.serial('no token provided', async (t) => {
-  t.plan(1);
   const {app} = t.context;
   const res = await request(app).get(`/${config.app.url}/dummy`);
   t.is(res.status, 401);
 });
 
-// test.serial('register:Success', async (t) => {
-//   t.plan(1);
-//   const {app} = t.context;
-// 	const res = await request(app)
-// 		.post(`/${config.app.url}/register`)
-// 		.send({username:'ava', password: '123123', email: 'ava@rocks.com'});
+test.serial('register:Success', async (t) => {
+  const {app} = t.context;
+	const res = await request(app)
+		.post(`/${config.app.url}/register`)
+		.send({username:'ava', password: '123123', email: 'ava@rocks.com'});
 
-//   console.log(res);
-//   console.log(res.body);
-// 	t.is(res.status, 201);
-// });
+	t.is(res.status, 201);
+});
+
+test.serial('authorize:Success', async (t) => {
+  const {app} = t.context;
+	const res = await request(app)
+		.post(`/${config.app.url}/authorize`)
+		.send({username:'ava', password: '123123'});
+
+  t.true(res.body.token !== undefined);
+  t.context.token = res.body.token;
+	t.is(res.status, 200);
+});
 
 test.afterEach.always(async () => {
   await mongo.quit();
-  // await mysql.quit();
+  await mysql.quit();
   await redis.quit();
 });
