e32e5f1f32885aece4cb8e2728c195e40afa25ed
TommyStarK <thomasmilox@gmail.com>
2020-03-13 15:22:08 +0100

    update test to allow concurent CI builds

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-docker-compose -f test/compose-test.yml --project-name boilerplate up --detach --build --force-recreate;
+docker-compose -f test/compose-test.yml --project-name boilerplate_${BUILD_NUMBER} up --detach --build --force-recreate;
 
-docker build . -t ava:latest;
+docker build . -t rest_api_boilerplate_test_with_ava_job_${BUILD_NUMBER};
 
-docker run -ti --rm --network=boilerplate_default -v `pwd`:/home -e MONGO_URI=mongodb -e MYSQL_URL=mysql \
--p 3001:3001 -p 8443:8443 ava:latest bash -c './scripts/wait_for_it.sh mysql:3306 -- yarn startAva; exit $?';
+docker run -ti --rm --network=boilerplate_${BUILD_NUMBER}_default -v `pwd`:/home -e MONGO_URI=mongodb -e MYSQL_URL=mysql \
+rest_api_boilerplate_test_with_ava_job_${BUILD_NUMBER} bash -c './scripts/wait_for_it.sh mysql:3306 -- yarn startAva; exit $?;';
 
 rc=$?;
 
-docker stop mongodb mysql;
-
-docker rm mongodb mysql;
-
-docker rmi ava:latest;
+docker-compose -f test/compose-test.yml --project-name boilerplate_${BUILD_NUMBER} down;
+docker rmi rest_api_boilerplate_test_with_ava_job_${BUILD_NUMBER};
+docker volume prune -f;
 
 exit $rc;
